9f0c77a604273ee644b2b1b74758faf226356566
Jason Hodges <jasonlhodges@gmail.com>
2023-11-30 14:46:52 -0500

    End course - add customers page table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,25 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { Metadata } from 'next';
+import { lusitana } from '@/app/ui/fonts';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/customers/table';
+import { fetchCustomers, fetchFilteredCustomers } from '@/app/lib/data';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+  return (
+    <div className="w-full">
+      <Table customers={customers} />
+    </div>
+  );
 }
